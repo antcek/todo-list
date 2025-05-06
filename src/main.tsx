cd953e822bcc0fc10f242f07a664235f82840819
Anton <antonprotasow69@gmail.com>
2025-05-06 18:54:04 +0300

    Remove SSR and convert to fully static app

--- src/main.tsx
+++ src/main.tsx
@@ -5,23 +5,13 @@ import { store } from "./app/store";
 import App from "./App";
 import "./styles/index.css";
 
-const renderApp = () => {
-  const rootElement = document.getElementById("root");
-  if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(
-      <React.StrictMode>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </React.StrictMode>,
-    );
-  }
-};
-
-if (typeof window !== "undefined") {
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", renderApp);
-  } else {
-    renderApp();
-  }
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  );
 }
