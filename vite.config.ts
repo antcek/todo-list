2c38ca2eccb4a2059e909a8316ce39abf29accb3
Anton <antonprotasow69@gmail.com>
2025-05-06 18:55:11 +0300

    Change base path to relative for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/todo-list/", // base path for GitHub Pages
+  base: "./", // относительный путь вместо "/todo-list/"
   plugins: [react()],
   resolve: {
     alias: {
